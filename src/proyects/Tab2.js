4df144debf8635dcc1bf17a575dfb659470d23cb
Axel Ramiro <axel.ramiro.92@gmail.com>
2017-02-28 04:18:08 -0600

    Added capitalOutflow dynamic table

--- src/proyects/Tab2.js
+++ src/proyects/Tab2.js
@@ -40,16 +40,8 @@ class Tab2 extends Component {
           expectedAnnualYield:  0,
           expectedUtility:      0
         },
-        capitalOutflow:{
-          totalCost:  0,
-          salePrice: 0,
-          salesCommission: 0,
-          utility: 0,
-          estimatedTime: 0,
-          yieldIn18Months: 0,
-          annualYield: 0
-        },
-        supplementaryData: [["",""],["",""]]
+        capitalOutflow: [],
+        supplementaryData: []
       }
     }
   }
@@ -70,88 +62,55 @@ class Tab2 extends Component {
     }
     return(
       <div className="tab-pane fade in active" id="tab2">
-          <div className="col-sm-6">
+        <div className="col-sm-6">
 
-              <div className="panel panel-primary">
-                  <table className="table table-hover" id="dev-table">
-                      <thead className="panel-heading" style={{backgroundColor: '#4B118E', color: 'white', borderRadious: '0px'}}>
-                          <tr>
-                            <th className="text-center" style={{fontFamily: 'AvenirNext-Bold'}}>CORRIDA FINANCIERA</th>
-                            <th className="text-center" style={{fontFamily: 'AvenirNext-Bold'}}>DATOS</th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td className="text-center" style={{fontFamily: 'AvenirNext-Medium' }}>Costo del departamento</td>
-                          <td className="text-center" style={{fontFamily: 'AvenirNext-Bold'}}>{ property.capitalOutflow.totalCost }</td>
-                        </tr>
-                        <tr>
-                          <td className="text-center" style={{fontFamily: 'AvenirNext-Medium' }}>Precio estimado de venta</td>
-                          <td className="text-center" style={{fontFamily: 'AvenirNext-Bold'}}>{ property.capitalOutflow.salePrice }</td>
-                        </tr>
-                        <tr>
-                          <td className="text-center" style={{fontFamily: 'AvenirNext-Medium' }}>Comisión de venta</td>
-                          <td className="text-center" style={{fontFamily: 'AvenirNext-Bold'}}>{ property.capitalOutflow.salesCommission }</td>
-                        </tr>
-                        <tr>
-                          <td className="text-center" style={{fontFamily: 'AvenirNext-Medium' }}>Utilidad</td>
-                          <td className="text-center" style={{fontFamily: 'AvenirNext-Bold'}}>{ property.capitalOutflow.utility }</td>
-                        </tr>
-                        <tr>
-                          <td className="text-center" style={{fontFamily: 'AvenirNext-Medium' }}>Rendimiento en 18 meses</td>
-                          <td className="text-center" style={{fontFamily: 'AvenirNext-Bold'}}>{ property.capitalOutflow.yieldIn18Months }</td>
-                        </tr>
-                        <tr>
-                          <td className="text-center" style={{fontFamily: 'AvenirNext-Medium' }}>Rendimiento anualizado</td>
-                          <td className="text-center" style={{fontFamily: 'AvenirNext-Bold'}}>{ property.capitalOutflow.annualYield }</td>
-                        </tr>
-                      </tbody>
-                  </table>
-              </div>
+          <div className="panel panel-primary">
+            <EdiTableRender data={property.capitalOutflow} />
+          </div>
 
-              <div className="col-sm-12">
+          <div className="col-sm-12">
 
-                  <div className="col-xs-12 col-sm-4" style={{paddingLeft: '0px', paddingRight: '0px'}}>
-                    <div className="panel text-center price-panel panel-purple" style={{borderRadius: '0px'}}>
-                      <div className="panel-body">
-                        <h5 className="text-center" style={{fontFamily: 'AvenirNext-Bold'}}>Objetivo de captación</h5>
-                        <p className="text-center" style={{fontFamily: 'AvenirNext-Medium' }}>{ currency(property.fixedData.objectiveFundraising)}</p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="col-xs-12 col-sm-4" style={{paddingLeft: '0px', paddingRight: '0px'}}>
-                    <div className="panel text-center price-panel panel-red" style={{borderRadius: '0px'}}>
-                      <div className="panel-body">
-                        <h5 className="text-center" style={{fontFamily: 'AvenirNext-Bold'}}>Rendimiento<br/>Anual</h5>
-                        <p className="text-center" style={{fontFamily: 'AvenirNext-Medium' }}>{property.fixedData.expectedAnnualYield + '%'}</p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="col-xs-12 col-sm-4" style={{paddingLeft: '0px', paddingRight: '0px'}}>
-                    <div className="panel text-center price-panel panel-purple" style={{borderRadius: '0px'}}>
-                      <div className="panel-body">
-                        <h5 className="text-center" style={{fontFamily: 'AvenirNext-Bold'}}>Utilidad<br/>Esperada</h5>
-                        <p className="text-center" style={{fontFamily: 'AvenirNext-Medium' }}>{property.fixedData.expectedUtility + '%'}</p>
-                      </div>
-                    </div>
-                  </div>
-              </div>
-
-              <div className="col-sm-12">
-                <div className="panel panel-primary">
-                  <EdiTableRender data={property.supplementaryData} />
+            <div className="col-xs-12 col-sm-4" style={{paddingLeft: '0px', paddingRight: '0px'}}>
+              <div className="panel text-center price-panel panel-purple" style={{borderRadius: '0px'}}>
+                <div className="panel-body">
+                  <h5 className="text-center" style={{fontFamily: 'AvenirNext-Bold'}}>Objetivo de captación</h5>
+                  <p className="text-center" style={{fontFamily: 'AvenirNext-Medium' }}>{ currency(property.fixedData.objectiveFundraising)}</p>
                 </div>
               </div>
+            </div>
+
+            <div className="col-xs-12 col-sm-4" style={{paddingLeft: '0px', paddingRight: '0px'}}>
+              <div className="panel text-center price-panel panel-red" style={{borderRadius: '0px'}}>
+                <div className="panel-body">
+                  <h5 className="text-center" style={{fontFamily: 'AvenirNext-Bold'}}>Rendimiento<br/>Anual</h5>
+                  <p className="text-center" style={{fontFamily: 'AvenirNext-Medium' }}>{property.fixedData.expectedAnnualYield + '%'}</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-xs-12 col-sm-4" style={{paddingLeft: '0px', paddingRight: '0px'}}>
+              <div className="panel text-center price-panel panel-purple" style={{borderRadius: '0px'}}>
+                <div className="panel-body">
+                  <h5 className="text-center" style={{fontFamily: 'AvenirNext-Bold'}}>Utilidad<br/>Esperada</h5>
+                  <p className="text-center" style={{fontFamily: 'AvenirNext-Medium' }}>{property.fixedData.expectedUtility + '%'}</p>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <div className="col-md-6">
-              <div>
-                <iframe src={`https://www.google.com/maps/embed/v1/place?q=${co[0]}++${co[1]}
-                &key=AIzaSyCjbpxgy_fpNywsVbmQ-WCSFgIGAGGBvCY`} width='100%' height="227" frameBorder="0" style={{border:0}}></iframe>
-              </div>
+          <div className="col-sm-12">
+            <div className="panel panel-primary">
+              <EdiTableRender data={property.supplementaryData} />
+            </div>
           </div>
+        </div>
+
+        <div className="col-md-6">
+          <div>
+            <iframe src={`https://www.google.com/maps/embed/v1/place?q=${co[0]}++${co[1]}
+                &key=AIzaSyCjbpxgy_fpNywsVbmQ-WCSFgIGAGGBvCY`} width='100%' height="227" frameBorder="0" style={{border:0}}></iframe>
+          </div>
+        </div>
       </div>
 
     )
