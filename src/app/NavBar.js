218395440fe2f36af1c62873becd95482f8d21d2
Axel Ramiro <axel.ramiro.92@gmail.com>
2017-03-12 05:56:40 -0600

    Fixed main navbar

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import { Navbar, Nav } from 'react-bootstrap'
 
 class NavBar extends Component {
   /*componentDidMount funcion que se ejecuta antes de montar el componente y obtiene la informacion de
@@ -26,72 +27,60 @@ class NavBar extends Component {
       backgroundColor: "#4b118e"
     }
     const stylen6={
-      margin: "9px -15px"
+      margin: "9px -30px"
     }
     return (
-
-      <nav className="navbar navbar-light bg-faded" style={stylen}>
-
-        <div className="hidden-xs hidden-sm col-xs-12 col-lg-12" style={{backgroundColor: '#81D742', width:'100%', height:'29px'}}>
-          <p style={stylen2}>
-            Atención al cliente | 01 (55) 8000 6555 |
-            <a style={stylen3} href="mailto:hola@inverspot.mx"> hola@inverspot.mx</a>
+        <Navbar bsStyle="none" collapseOnSelect className="navbar navbar-light bg-faded" style={stylen}>
+          <div className="hidden-xs hidden-sm col-xs-12 col-lg-12" style={{backgroundColor: '#81D742', width:'100%', height:'29px'}}>
+            <p style={stylen2}>
+              Atención al cliente | 01 (55) 8000 6555 |
+              <a style={stylen3} href="mailto:hola@inverspot.mx"> hola@inverspot.mx</a>
 
 
-            <a style={{float:'right', marginRight: '100px', marginLeft: '10px', color: 'white',}} href="https://vimeo.com/190621870" target="_blank"><i className="fa fa-vimeo" aria-hidden="true"></i></a>
+              <a style={{float:'right', marginRight: '100px', marginLeft: '10px', color: 'white',}} href="https://vimeo.com/190621870" target="_blank"><i className="fa fa-vimeo" aria-hidden="true"></i></a>
 
-            <a style={stylen4} href="https://www.waze.com/es-419/livemap?zoom=17&lat=19.42268&lon=-99.22286" target="_blank">
-              <i className="fa fa-crosshairs" aria-hidden="true"></i></a>
+              <a style={stylen4} href="https://www.waze.com/es-419/livemap?zoom=17&lat=19.42268&lon=-99.22286" target="_blank">
+                <i className="fa fa-crosshairs" aria-hidden="true"></i></a>
 
-            <a style={stylen4} href="https://www.youtube.com/channel/UCvr85MwH36lbTAvOdmeDtxg" target="_blank">
-              <i className="fa fa-youtube" aria-hidden="true"></i></a>
+              <a style={stylen4} href="https://www.youtube.com/channel/UCvr85MwH36lbTAvOdmeDtxg" target="_blank">
+                <i className="fa fa-youtube" aria-hidden="true"></i></a>
 
-            <a style={stylen4} href="https://twitter.com/Inverspot2017" target="_blank">
-              <i className="fa fa-twitter" aria-hidden="true"></i></a>
+              <a style={stylen4} href="https://twitter.com/Inverspot2017" target="_blank">
+                <i className="fa fa-twitter" aria-hidden="true"></i></a>
 
-            <a style={stylen4} href="https://www.facebook.com/inverspot" target="_blank">
-              <i className="fa fa-facebook" aria-hidden="true"></i></a>
-          </p>
-        </div>
+              <a style={stylen4} href="https://www.facebook.com/inverspot" target="_blank">
+                <i className="fa fa-facebook" aria-hidden="true"></i></a>
+            </p>
+          </div>
+          <Navbar.Header className="navbar-header top-logo-holder">
+            <Link to="/">
+              <div className="icon-logo-main"></div>
+            </Link>
+            <p>Tu punto de inversión inmobiliario</p>
+            <Navbar.Toggle style={stylen5}>
+              <i className="fa fa-bars" aria-hidden="true"></i>
+            </Navbar.Toggle>
+          </Navbar.Header>
+          <Navbar.Collapse className="navbar-collapse collapse top-tittle-holder">
+            <Nav>
+              <li><Link to="/nosotros">Nosotros</Link></li>
+              <li><Link to="/proyectos">Proyectos</Link></li>
+              <li><Link to="/como-funciona">Como Funciona</Link></li>
+              <li><Link to="/garantia">Garantias</Link></li>
+              <li><Link to="/faqs">FAQ´s</Link></li>
+              <li><Link to="/contacto">Contacto</Link></li>
+              { !this.user &&
+                <li onClick={() => this.props.stepSet(1)} style={{cursor: "pointer"}}><a>Regístro</a></li>
+              }
+              { !this.user &&
+                <li onClick={() => this.props.stepSet(2)} style={{cursor: "pointer"}}><a>Inicio de Sesión</a></li>
+              }
 
-        <div className="navbar-header top-logo-holder">
-          <Link to="/">
-            <div className="icon-logo-main"></div>
-          </Link>
-          <p>Tu punto de inversión inmobiliario</p>
-          <button style={stylen5} type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <i className="fa fa-bars" aria-hidden="true"></i>
-          </button>
-        </div>
-
-
-        <div className="navbar-collapse collapse top-tittle-holder">
-          <ul className="nav navbar-nav" style={stylen6}>
-            <li>
-              <Link to="/nosotros">Nosotros</Link>
-            </li>
-            <li>
-              <Link to="/proyectos">Proyectos</Link>
-            </li>
-            <li><Link to="/como-funciona">Como Funciona</Link></li>
-            <li><Link to="/garantia">Garantias</Link></li>
-            <li><Link to="/faqs">FAQ´s</Link></li>
-            <li><Link to="/contacto">Contacto</Link></li>
-            {!this.user &&
-              <li id='reg'><a data-toggle="modal" onClick={() => this.props.stepSet(1)} style={{cursor: "pointer"}}>Regístro</a></li> }
-            {!this.user &&
-              <li id='acc'><a data-toggle="modal" onClick={() => this.props.stepSet(2)} style={{cursor: "pointer"}}>Inicio de Sesión</a></li> }
-            {this.user &&
-              <li>
-                <Link to="/user/profile">
-                  Mi perfil
-                </Link>
-              </li> }
-
-          </ul>
-        </div>
-      </nav>
-
+              {this.user &&
+                <li><Link to="/user/profile">Mi perfil</Link></li> }
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
     )
   }
 }
