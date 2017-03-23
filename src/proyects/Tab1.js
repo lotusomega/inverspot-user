78a9366ce4749310f6761e5d136375b01273f5e5
Axel Ramiro <axel.ramiro.92@gmail.com>
2017-03-23 11:54:55 -0600

    Added dangerous html

--- src/proyects/Tab1.js
+++ src/proyects/Tab1.js
@@ -28,22 +28,20 @@ class Tab1 extends Component {
     let property = this.state.property
     return (
       <div className="tab-pane fade in active" id="tab1">
-          <div className="col-sm-6">
-             <h2 className="" style={{fontWeight: '700', marginTop: '0px'}}>Resumen del Proyecto</h2>
-             <h3 style={{fontWeight: '700', marginTop: '-10px'}}>{property.title}</h3>
-          </div>
-          <div className="col-sm-6">
-              <h4 style={{float: 'right', fontWeight: '700'}}>{property.address.suburb}</h4><br/><br/>
-              <h5 style={{float: 'right', fontWeight: '400', marginTop: '-8px'}}>Proyecto de {property.title}</h5>
-          </div>
+        <div className="col-sm-6">
+          <h2 className="" style={{fontWeight: '700', marginTop: '0px'}}>Resumen del Proyecto</h2>
+          <h3 style={{fontWeight: '700', marginTop: '-10px'}}>{property.title}</h3>
+        </div>
+        <div className="col-sm-6">
+          <h4 style={{float: 'right', fontWeight: '700'}}>{property.address.suburb}</h4><br/><br/>
+          <h5 style={{float: 'right', fontWeight: '400', marginTop: '-8px'}}>Proyecto de {property.title}</h5>
+        </div>
+        <div className="spacer double"></div>
+        <div className="col-sm-12 resumen-list" id="resume">
           <div className="spacer double"></div>
-          <div className="col-sm-12 resumen-list" id="resume">
-             <div className="spacer double"></div>
-              <p className="text-justify">
-                {property.description}
-              </p>
-              <div className="spacer double"></div>
-          </div>
+          <p className="text-justify" dangerouslySetInnerHTML={{__html: property.description}} />
+          <div className="spacer double"></div>
+        </div>
       </div>
     )
   }
